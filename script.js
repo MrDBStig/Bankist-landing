033fd5b8d6c2657cba63981846353e3c0fd51fb0
mikita kavaleuski <nikcrafter1999@gmail.com>
2021-12-28 22:13:18 +0300

    Implemented smooth scrolling

--- script.js
+++ script.js
@@ -29,3 +29,17 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+// Smooth scrolling
+const btnScrollTo = document.querySelector('.btn--scroll-to'),
+  section1 = document.querySelector('#section--1');
+btnScrollTo.addEventListener('click', function (e) {
+  const s1coords = section1.getBoundingClientRect();
+  // Scrolling
+  // window.scrollTo({
+  //   left: s1coords.left + window.pageXOffset,
+  //   top: s1coords.top + window.pageYOffset,
+  //   behavior: 'smooth',
+  // });
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
