68ca089a094424e11edddbbb4b064193a077fe83
mikita kavaleuski <nikcrafter1999@gmail.com>
2021-12-29 19:01:20 +0300

    Implemented fade in aniamtion on scroll

--- script.js
+++ script.js
@@ -118,3 +118,21 @@ const headerObserver = new IntersectionObserver(stickyNav, {
   rootMargin: `-${navHeight}px`,
 });
 headerObserver.observe(header);
+
+///////////////////////////////////////
+// Fade in navigation on scroll (Reveal sections)
+const allSections = document.querySelectorAll('.section');
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove('section--hidden');
+  sectionObserver.unobserve(entry.target);
+};
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+allSections.forEach(section => {
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden');
+});
